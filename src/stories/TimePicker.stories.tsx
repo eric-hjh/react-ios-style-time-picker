39e2b446d6716ff928ce36aee86bb6ca8d1e5d84
Eric <hgh1057@gmail.com>
2025-03-18 16:41:33 +0900

    update storybook

--- src/stories/TimePicker.stories.tsx
+++ src/stories/TimePicker.stories.tsx
@@ -2,7 +2,7 @@ import { StoryFn } from '@storybook/react';
 import TimePicker, { TimePickerProps } from '../components/TimePicker';
 
 export default {
-  title: 'Example/TimePicker',
+  title: 'TimePicker',
   component: TimePicker,
 };
 
@@ -11,7 +11,16 @@ const Template: StoryFn<TimePickerProps> = (args: TimePickerProps) => (
 );
 
 export const Default = Template.bind({});
-export const Infinite = Template.bind({});
-Infinite.args = {
+export const DefaultInfinite = Template.bind({});
+DefaultInfinite.args = {
   infinite: true,
+};
+export const TwentyFourHourFormat = Template.bind({});
+TwentyFourHourFormat.args = {
+  hourFormat: '24',
+};
+export const TwentyFourHourFormatInfinite = Template.bind({});
+TwentyFourHourFormatInfinite.args = {
+  infinite: true,
+  hourFormat: '24',
 };
